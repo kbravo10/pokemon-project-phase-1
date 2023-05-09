32d76f47e2ab2906195757cb4fdc8006e854a7aa
Kevin Bravo <kbravo1024@gmail.com>
2023-05-08 21:26:15 -0700

    add real animals part 2

--- scripts/main.js
+++ scripts/main.js
@@ -24,9 +24,10 @@ document.addEventListener("DOMContentLoaded",(event) =>{
     //add the fetch GET json data to the website using function
     function addPokeCards(pokeCard){
         const div = document.getElementById("pokemon-images")
-
+        const divReal = document.getElementById("real-img")
         const pokemonChar = document.createElement("div");
         const img = document.createElement("img");
+        const realImg = document.createElement("img");
         const btn = document.createElement("button");
 
         pokemonChar.setAttribute("class", "chooseEm")
@@ -34,11 +35,25 @@ document.addEventListener("DOMContentLoaded",(event) =>{
         img.setAttribute("src", pokeCard["image"]);
         img.setAttribute("class", "pokeImg");
 
+        realImg.setAttribute("src", pokeCard["real"]);
+
         btn.setAttribute("id", pokeCard["id"]);
         btn.setAttribute("class", "pokeButton");
-        
+
         btn.appendChild(img);
         pokemonChar.appendChild(btn);
-        div.appendChild(pokemonChar);
+        div.appendChild(pokemonChar)
+    
+        //adding event listenerv for buitton
+        pokemonChar.addEventListener("click", (e)=>{
+            e.preventDefault();
+            document.querySelector("form.add-pokemon-form").style.display = "none";
+            realLifeDisplay(realImg)
+        })
+}
+    function realLifeDisplay(realPokemon){
+        const realDiv = document.getElementById("real-img");
+        realDiv.setAttribute("src", realPokemon.src)
     }
+       
 })
